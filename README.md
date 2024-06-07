0d874ce54f4cead7db651b6ebe8ad6e0901c7b20
Nasseima <lindsaynasseima@gmail.com>
2024-06-07 11:00:35 -0400

    I organized my project, Fix my readMe file and removed commented out code, create a for loop to iterate over the songs, and I added Bom methods and attributes

---
+++ README.md
@@ -0,0 +1,2 @@
+I created a web page that accesses my favorite songs. 
+On this page you're able to subscribe and request songs.
--- app.js
+++ app.js
@@ -1,46 +1,38 @@
-// const audio = new Audio ();
-// audio.src = "Chronic Law - Uncomfort To Lies _ Audio (256).mp3"
-// const button = document.querySelector("button");
+const audio = document.getElementsByClassName('songbtn');
+const playPauseBTN = document.getElementsByClassName('playPauseBTN');
+const stopBtn = document.getElementsByClassName('stop')
 
-// button.forEach(button =>{
-//     button.addEventlistener("click", () => {
-//         audio.play();
-//     });
-// });
-//Code from html added here !!
-//<button onmousedown="audio.play()" onmouseleave="audio.stop()" class="btn"><img width="50px" height="50px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgtsKxTUuqOwPPgUvMll3w99Gd8DcS7uv-og&s" alt=""> </button>
-
-const audio = document.querySelector('.songbtn');
-const playPauseBTN = document.querySelector ('.playPauseBTN');
-let count = 0;
-
-console.dir(audio)
-
-function playPause(){
-    if (count == 0) {
-        count = 1;
-        audio.play();
-        playPauseBTN.innerHTML = "Pause &#9208;";
-    }else{
-        count = 0;
-        audio.pause();
-        playPauseBTN.innerHTML = "Play &#9658;";
-    }
-}
-function stop(){
-    playPause();
-    audio.pause();
-    audio.currentTime = 0;
-    console.log(audio.currentTime)
-    playPauseBTN.innerHTML = "Play &#9658;";
+function pause(index) {
+    audio[index].pause();
+    audio[index].currentTime = 0;
 }
 
+function play(index){
+    audio[index].play();
+}
 
-const card = document.querySelector(".back")
+for(let i = 0; i < playPauseBTN.length; i++) {
+    playPauseBTN[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        play(i)
+    })
+} 
+
+for(let i = 0; i < stopBtn.length; i++) {
+    stopBtn[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        pause(i)
+    })
+} 
+
+
+
+const card = document.querySelector("#back")
 const audio1 = document.querySelector(".songbtn")
 card.append(audio1)
 
-console.log(card, audio1)
 
 let name = document.getElementById('name');
-console.log(name)
+
+window.open("https://music.apple.com/us/browse", "popup")
+const windowFeatures = "left=100, top=100, width=100, height=100"
